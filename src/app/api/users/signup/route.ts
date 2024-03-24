705aebe45e2721bfd5ffa4217b2b06e1c381cb84
ShuhbamUpadhyayDeveloper <shubhamwork48@gmail.com>
2024-03-24 11:00:48 +0530

    testing complete

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,34 +1,39 @@
 import User from "@/models/user.model";
-import {NextRequest ,NextResponse} from  'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import bcryptjs from 'bcryptjs'
 import { connectToMongodb } from "@/dbConnect/connet";
 import { TsignUpApi } from "@/types/types";
+import { nodeMailer } from "@/utils/nodemailer";
 
 connectToMongodb()
 
-export async function POST(request:NextRequest) {
+export async function POST(request: NextRequest) {
    try {
 
-     const reqBody:TsignUpApi = await request.json()
-     const {username,email,password} = reqBody
-     if(!username && !email && !password) return NextResponse.json({error : "Value is missing in signupapi"},{status : 400})
+      const reqBody: TsignUpApi = await request.json()
+      const { username, email, password } = reqBody
 
-    const user = await User.find({username})
-    if(user) return NextResponse.json({message : "user already exist"},{status:409})
+      if (!username || !email || !password) return NextResponse.json({ error: "Value is missing in signupapi" }, { status: 400 })
 
-    const salt = await bcryptjs.genSalt(10)
-    const hashedPassword =  await bcryptjs.hash(password,salt)
+      const user = await User.findOne({ email })
+      if (user) return NextResponse.json({ message: "user already exist" }, { status: 409 })
 
-    const newUser = new User({
-        username,
-        email,
-        password:hashedPassword
-    })
+      const salt = await bcryptjs.genSalt(10)
+      const hashedPassword = await bcryptjs.hash(password, salt)
 
-    await newUser.save()
+      const newUser = new User({
+         username,
+         email,
+         password: hashedPassword
+      })
 
-    return NextResponse.json(newUser)
+      await newUser.save()
+
+      // sendEmail to user
+      await nodeMailer({ email, emailType: 'VERIFY', userId: newUser._id })
+
+      return NextResponse.json({ newUser, success: true })
    } catch (error) {
-      NextResponse.json({error:"Error in Singupapi" + error},{status:500})
+      NextResponse.json({ error: "Error in Singupapi" + error }, { status: 500 })
    }
 }
--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -1,5 +1,44 @@
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
+import bcryptjs from 'bcryptjs'
+import User from '@/models/user.model';
 
-async function main() {
+export async function nodeMailer({ email, emailType, userId }:any) {
+   try {
+       const hashedToken = await bcryptjs.hash(userId.toString(),10)
+       if(emailType ===  'VERIFY') {
+            await User.findByIdAndUpdate(userId, {
+               verifyToken : hashedToken,
+               verifyTokenExpiry : Date.now() + 3600000
+           })
+       } else if(emailType === "RESET") {
+           await User.findByIdAndUpdate(userId, {
+               forgotPasswordToken :hashedToken,
+               forgotPasswordTokenExpiry : Date.now() + 3600000
+           })
+       }
+   
+       const transport = nodemailer.createTransport({
+           host: "sandbox.smtp.mailtrap.io",
+           port: 2525,
+           auth: {
+               user: process.env.MAIL_TRAP_USER,
+               pass: process.env.MAIL_TRAP_PASSWORD
+           }
+         });
+   
+       const mailOptions = {
+           from: '"mrshubhamu@gmail.com',
+           to: email, 
+           subject: emailType === 'VERIFY' ? 'verify your email' : "Reset your password", 
+           html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+           or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+           </p>`, // html body
+       }
+       
+       const mailResponse = await transport.sendMail(mailOptions);
+       return mailResponse
     
+   } catch (error) {
+      console.log('Error in SendEmail',error)
+   }
 }
